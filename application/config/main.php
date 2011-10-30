1855bec45f0f6d3abeff652b517628d968495302
E.Karagodin <ekaragodin@gmail.com>
2011-10-30 16:36:59 +0700

    added array production hosts

--- application/config/main.php
+++ application/config/main.php
@@ -1,58 +1,62 @@
 <?php
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+$productionHosts = array();
+
+if (in_array($_SERVER['HTTP_HOST'], $productionHosts)) {
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+} else {
+    defined('YII_DEBUG') or define('YII_DEBUG', false);
+}
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 $config = array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'My Web Application',
 
-	// preloading 'log' component
-	'preload'=>array('log'),
+    // preloading 'log' component
+    'preload' => array('log'),
 
-	// autoloading model and component classes
-	'import'=>array(
-		'application.models.*',
-		'application.components.*',
-	),
+    // autoloading model and component classes
+    'import' => array(
+        'application.models.*',
+        'application.components.*',
+    ),
 
-	'modules'=>array(
-	),
+    'modules' => array(
+    ),
 
-	// application components
-	'components'=>array(
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
-			),
-		),
-	),
+    // application components
+    'components' => array(
+        'db' => array(
+            'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+            'emulatePrepare' => true,
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ),
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
+                ),
+                // uncomment the following to show log messages on web pages
+                /*
+                    array(
+                        'class'=>'CWebLogRoute',
+                    ),
+                    */
+            ),
+        ),
+    ),
 
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
-	'params'=>array(
-		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
-	),
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
+    'params' => array(
+        // this is used in contact page
+        'adminEmail' => 'webmaster@example.com',
+    ),
 );
